3263e88dc771669ab360c00717ccba9e7afb3d11
Mootassam <mootassame@gmail.com>
2021-09-30 23:09:00 +0100

    add Error Number

--- src/view/shared/errors/Error404.tsx
+++ src/view/shared/errors/Error404.tsx
@@ -7,7 +7,7 @@ const Error404Page = () => {
       <div id='notfound'>
         <div className='notfound'>
           <div className='notfound-404'>
-            <h1>404</h1>
+            <h1>400</h1>
           </div>
           <h2>We are sorry, Page not found!</h2>
           <p>
