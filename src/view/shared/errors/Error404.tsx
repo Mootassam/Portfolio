d027a7cdfc329c5eac6e7a49ba25988b48617dc7
Mootassam <mootassame@gmail.com>
2021-10-27 23:39:42 +0100

    add some text

--- src/view/shared/errors/Error404.tsx
+++ src/view/shared/errors/Error404.tsx
@@ -14,7 +14,7 @@ const Error404Page = () => {
             The page you are looking for might have been removed had its name
             changed or is temporarily unavailable.
           </p>
-          <Link to={"/"}>Back To Homepage</Link>
+          <Link to={"/"}>Back To Home page</Link>
         </div>
       </div>
     </ErrorWrapper>
